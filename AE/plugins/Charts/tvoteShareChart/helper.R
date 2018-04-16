6d0e16e52f7d6551b794f60e7e554e28681609f4
mohitkharb <mohitkharb@gmail.com>
2018-04-16 13:13:06 +0530

    updated party wise colors for AE Charts.

--- AE/plugins/Charts/tvoteShareChart/helper.R
+++ AE/plugins/Charts/tvoteShareChart/helper.R
@@ -14,12 +14,17 @@
     #browser()
     pivotdata<-dcast(b,Year~Party,value.var=c('Votes'))
 
+
+    col <- read.csv("../tcpd_data/data/colours.csv")
+    pal <- as.character(col$Color)
+    pal <- setNames(pal,col$Party)
+
     #create a base line chart with year as the x-axis
     base<-plot_ly(pivotdata, x = ~Year)
     #print(paste('selected',selectedpartynames))
     # #for each selected party in the input "filter_pname" id (checkbox) add a new trace
     # #corresponding to that party
-    lapply(selectedpartynames,function(x) {print(paste('adding',x));base<<-add_trace(base,y=~get(x),name=x,type='scatter',mode='lines+markers')})
+    lapply(selectedpartynames,function(x) {print(paste('adding',x));base<<-add_trace(base,y=~get(x),type='scatter',mode='lines+markers',color=x,colors=pal)})
     sname<-gsub("_"," ",sname)
     thistitle<-paste0('Party wise voteshare across years in ',sname)
     xtitle<-''
