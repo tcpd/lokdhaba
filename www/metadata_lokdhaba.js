6b9a678c9393373ec732df94b496038d0b0bf72c
salonibhogale <saloni_bhogale@hotmail.com>
2018-11-19 12:43:42 +0530

    added file for Google Dataset Discovery

---
+++ www/metadata_lokdhaba.js
@@ -0,0 +1,34 @@
+<script type="application/ld+json">
+{
+  "@context":"http://schema.org/",
+  "@type":"Dataset",
+  "name":"TCPD: Indian Elections Result Data",
+  "description":"The Indian General & State Assembly Election and Candidates Database 1961- today ",
+  "url":"http://lokdhaba.ashoka.edu.in/LokDhaba-Shiny/",
+  "sameAs":"https://gis.ncdc.noaa.gov/geoportal/catalog/search/resource/details.page?id=gov.noaa.ncdc:C00510",
+  "keywords":[
+     "INDIAN ELECTIONS > GENERAL ELECTIONS > RESULTS",
+     "INDIAN ELECTION > GENERAL ELECTIONS > CANDIDATES",
+     "INDIAN ELECTION > STATE ELECTION > RESULTS",
+     "INDIAN ELECTION > STATE ELECTION > CANDIDATES"
+  ],
+  "creator":{
+     "@type":"Trivedi Centre for Political Data",
+     "url": "http://tcpd.ashoka.edu.in",
+     "name":"TCPD: Indian Elections Result Data",
+     "contactPoint":{
+        "@type":"ContactPoint",
+        "contactType": "User service",
+        "email":"tcpd-contact@ashoka.edu.in"
+     }
+  },
+  "distribution":[
+     {
+        "@type":"DataDownload",
+        "encodingFormat":"CSV",
+        "contentUrl":"http://lokdhaba.ashoka.edu.in/LokDhaba-Shiny/"
+     },
+  ],
+  "temporalCoverage":"1962-01-01/2018-11-05",
+}
+</script>
