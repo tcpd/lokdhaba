0ac3a290c3f597e46d5ed9aa98a056630ed53b62
hangal <hangal@stanfordalumni.org>
2019-05-27 20:45:31 +0530

    loop to generate data file for all assembly #s.

--- www/incumbency/csv-gen.r
+++ www/incumbency/csv-gen.r
@@ -7,29 +7,32 @@ filePath <- '~/tcpd_data/GE/Data/derived/mastersheet.csv'
 filePath = '~/tcpd_data/GE/GE_19_working/mastersheet_with_adr_data.csv'
 data = fread(filePath)
 #dt <- dt[dt$No_Mandates > 0]
-dt <- data[Party != 'NOTA' & Candidate != 'NOTA', c("Assembly_No", "Poll_No", "Year", "Candidate", "State_Name", "Constituency_Name", "Party", "Last_Party", "pid", "Votes", "Sex", "Position", "Contested", "No_Mandates", "Turncoat", "Incumbent", "Vote_Share_Percentage", "Margin", "Margin_Percentage", "MyNeta_age")]
+data <- data[Party != 'NOTA' & Candidate != 'NOTA', c("Assembly_No", "Poll_No", "Year", "Candidate", "State_Name", "Constituency_Name", "Party", "Last_Party", "pid", "Votes", "Sex", "Position", "Contested", "No_Mandates", "Turncoat", "Incumbent", "Vote_Share_Percentage", "Margin", "Margin_Percentage", "MyNeta_age")]
 
 # filter dt down to only rows whose pid is present in this assembly
-assembly = 17
-dt = dt[Assembly_No <= assembly] # filter out all rows after this assembly
-
-# get pids of everyone who has a line in this assembly, but drop INDs > 3 to avoid the long tail of insignificant cands
-this_assembly_pids = unique(dt[Assembly_No == assembly & !(Party == 'IND' & Position > 3)]$pid)
-dt = dt[pid %in% this_assembly_pids]
-# only keep those rows with a pid in this assembly
-
-terms_served_by_pid = dt[Position == 1, .(Terms=length(unique(Assembly_No))), by=c('pid')]
-dt = merge (dt, terms_served_by_pid, by=c('pid'), all.x=TRUE)
-
-# fix the #mandates and contested to be whatever it is up to the current assembly
-# otherwise rows for the same pid will have different values in these columns
-dt[, No_Mandates:=max(No_Mandates), by=c('pid')]
-dt[, Contested:=max(Contested), by=c('pid')]
-
-terms_contested_by_pid = dt[, .(Terms_Contested=length(unique(Assembly_No))), by=c('pid')]
-dt = merge (dt, terms_contested_by_pid, by=c('pid'), all.x = TRUE)
-
-fwrite(dt, file='rows-ge.csv')
+for (assembly in min(data$Assembly_No):max(data$Assembly_No)) {
+  print (paste('Generating data for assembly# ', assembly))
+  
+  dt = data[Assembly_No <= assembly] # filter out all rows after this assembly
+  
+  # get pids of everyone who has a line in this assembly, but drop INDs > 3 to avoid the long tail of insignificant cands
+  this_assembly_pids = unique(dt[Assembly_No == assembly & !(Party == 'IND' & Position > 3)]$pid)
+  dt = dt[pid %in% this_assembly_pids]
+  # only keep those rows with a pid in this assembly
+  
+  terms_served_by_pid = dt[Position == 1, .(Terms=length(unique(Assembly_No))), by=c('pid')]
+  dt = merge (dt, terms_served_by_pid, by=c('pid'), all.x=TRUE)
+  
+  # fix the #mandates and contested to be whatever it is up to the current assembly
+  # otherwise rows for the same pid will have different values in these columns
+  dt[, No_Mandates:=max(No_Mandates), by=c('pid')]
+  dt[, Contested:=max(Contested), by=c('pid')]
+  
+  terms_contested_by_pid = dt[, .(Terms_Contested=length(unique(Assembly_No))), by=c('pid')]
+  dt = merge (dt, terms_contested_by_pid, by=c('pid'), all.x = TRUE)
+  
+  fwrite(dt, file=paste0('ge-incumbency-',assembly,'.csv'))
+}
 
 # read the pics table from both LS and PRS and assign the link in the last row to that pid
 pics_ls = fread ('~/tcpd_data/GE/GE_19_working/pictures_with_pids.csv');
